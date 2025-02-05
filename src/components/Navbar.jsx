187c8b2cb4d97804637954cc86b4d2ec6d747c60
Froyd <banataofroyd@gmail.com>
2025-02-05 18:08:56 +0800

    revised code for pagination and navBar cpmponent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import GarageOutlinedIcon from '@mui/icons-material/GarageOutlined';
 import PaidIcon from '@mui/icons-material/Paid';
@@ -8,62 +8,59 @@ import '../components/Nav.css'
 
 export default function Navbar(){
 
-    const [active, setActive] = useState(["navBtn active", "navBtn", "navBtn", "navBtn"]);
-    const [pageName, setPageName] = useState("Dashboard");
+    const navigationBtn = [
+        {
+            btnName:'Dashboard', 
+            path: '/',
+            btnIcon: <DashboardIcon sx={{fontSize: 25}}/>
+        },
+        {
+            btnName:'Slots', 
+            path: '/slot',
+            btnIcon:  <GarageOutlinedIcon sx={{fontSize: 25}}/>
+        },
+        {
+            btnName:'Client', 
+            path: '/client',
+            btnIcon: <PaidIcon sx={{fontSize: 25}}/>
+        },
+        {
+            btnName:'Transaction', 
+            path: '/',
+            btnIcon: <PeopleAltIcon sx={{fontSize: 25}}/>
+        }
+    ];
+
+    const activePageRef = useRef([]);
+    const [clickedIndex, setClickedIndex] = useState(0);
+
+    useEffect(() => {
+        activePageRef.current[clickedIndex].classList.add('active');
+    }, [clickedIndex])
+
     
-        //active.current.classList.add("active")
-    function setDashboardClass(){
-        setActive(a => ["navBtn active", "navBtn", "navBtn", "navBtn"])
-        setPageName("Dashboard");
-    }
-
-    function setSlotClass(){
-        setActive(a => ["navBtn", "navBtn active", "navBtn", "navBtn"])
-        setPageName("Available Slot")
-    }
-
-    function setClientClass(){
-        setActive(a => ["navBtn", "navBtn", "navBtn active", "navBtn"])
-        setPageName("Client Type")
-    }
-
-    function setTransactionClass(){
-        setActive(a => ["navBtn", "navBtn", "navBtn", "navBtn active"])
-        setPageName("Transaction History")
-    }
-
-    function activeIcon(number){
-        return active[number] ==="navBtn active"?"action":"";
+    function handleActiveBtn(index){
+        activePageRef.current.forEach(element => {
+            if(element.classList.contains('active')){
+                element.classList.remove('active')
+            }
+        })
+        setClickedIndex(index);
     }
 
     return(<>
         <nav className="nav-container">
         <h1 id="name">ParkEase</h1>
         <hr/>
-        <Link to='/'>
-            <div className={active[0]} onClick={setDashboardClass}>
-                <DashboardIcon color={activeIcon(0)} sx={{fontSize: 25}}/>
-                <span>Dashboard</span>
-            </div>
-        </Link>
-        <Link to='/slot'>
-            <div className={active[1]} onClick={setSlotClass}>
-                <GarageOutlinedIcon color={activeIcon(1)} sx={{fontSize: 25}}/>
-                <span>Slots</span>
-            </div>
-        </Link>
-        <Link to='/client'>
-            <div className={active[2]} onClick={setClientClass}>
-                <PeopleAltIcon color={activeIcon(2)} sx={{ fontSize: 25 }}/>
-                <span>Client</span>
-            </div>
-        </Link>
-        <Link to=''>
-            <div className={active[3]} onClick={setTransactionClass}>
-                <PaidIcon color={activeIcon(3)} sx={{ fontSize: 25 }}/>
-                <span>Transaction</span>
-            </div>
-        </Link>
+        {navigationBtn.map((item, index) => 
+            <Link key={index} to={item.path} onClick={() => handleActiveBtn(index)}>
+                <div className='navBtn' 
+                    ref={(el) => activePageRef.current[index] = el}>
+                    {item.btnIcon}
+                    <span>{item.btnName}</span>
+                </div>
+            </Link>
+        )}
       </nav>
       </>
     )
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,9 +4,9 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function Pagination(props){
 
-    const limitPage = 4;
-    const pageNumber = Math.ceil(props.numberOfData / props.maxPerPage);
-    const totalPage = pageNumber > limitPage ? limitPage : pageNumber;
+    const LIMIT_PAGE = 4;
+    const PAGE_NUMBER = Math.ceil(props.numberOfData / props.maxPerPage);
+    const TOTAL_PAGE = PAGE_NUMBER > LIMIT_PAGE ? LIMIT_PAGE : PAGE_NUMBER;
 
     let activeRef = useRef([]);
     const [index, setIndex] = useState(0);
@@ -21,27 +21,24 @@ export default function Pagination(props){
     }, [index])
 
     useEffect(() => {
-        activeRef.current = activeRef.current.filter(value => {
-            return value !== null;
-        });
-        setIndex(i => i = 0);
-        setCount(c => c = 0);
-    }, [pageNumber])
+        activeRef.current = activeRef.current.filter(value => value !== null);
+        setIndex(0);
+        setCount(0);
+    }, [PAGE_NUMBER])
 
     const [adjustCount, setCount] = useState(0);
-
     function handleNextBtn(){
         props.nextClick();
 
-        if(index < totalPage-1){
+        if(index < TOTAL_PAGE-1){
             setIndex(i => i + 1);
-        }else if(pageNumber > limitPage && index == (limitPage-1) && pageNumber !== (adjustCount+limitPage)){
+        }else if(PAGE_NUMBER > LIMIT_PAGE && index == (LIMIT_PAGE-1) && PAGE_NUMBER !== (adjustCount+LIMIT_PAGE)){
             setCount(c => c + 1);
         }else return;
     }
 
     function handlePrevBtn(){
-        const rear = adjustCount+limitPage;
+        const rear = adjustCount+LIMIT_PAGE;
         const current = (index+1)+adjustCount;
         const front = rear-3;
 
@@ -57,7 +54,7 @@ export default function Pagination(props){
         <div id="pagination-controls"> 
         <ArrowBackIosNewIcon onClick={handlePrevBtn} color='primary' sx={{fontSize:15}}/>
          <button onClick={handlePrevBtn} id="prevPageBtn" className="pagination-btn" >previous</button>
-            {Array.from({length: totalPage || 1}, (_, index) => (
+            {Array.from({length: TOTAL_PAGE || 1}, (_, index) => (
                 <span 
                     key={index} 
                     className='pageInfo' 
